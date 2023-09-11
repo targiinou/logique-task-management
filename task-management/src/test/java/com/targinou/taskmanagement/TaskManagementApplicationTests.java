d40cb31f417737e52ad1614167a286e16abd4d46
Emanuel Targino <emanuel.targino@ehorus.com.br>
2023-09-11 10:40:01 -0300

    Adicionando docker-compose

--- task-management/src/test/java/com/targinou/taskmanagement/TaskManagementApplicationTests.java
+++ task-management/src/test/java/com/targinou/taskmanagement/TaskManagementApplicationTests.java
@@ -1,13 +1,9 @@
 package com.targinou.taskmanagement;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaskManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
