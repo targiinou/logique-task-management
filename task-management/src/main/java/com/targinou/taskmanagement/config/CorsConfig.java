c4fe259cefc364c8a0c79598622fdac57ca0c2b5
Emanuel Targino <emanuel.targino@ehorus.com.br>
2023-09-10 23:59:26 -0300

    configurando cors

--- task-management/src/main/java/com/targinou/taskmanagement/config/CorsConfig.java
+++ task-management/src/main/java/com/targinou/taskmanagement/config/CorsConfig.java
@@ -12,9 +12,10 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("*")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 
 }
